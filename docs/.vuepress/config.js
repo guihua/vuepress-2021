a84a6e0bc7f8da9a66d8a250413e873103903c18
龙趸 <guihuapeng@126.com>
2021-12-05 21:24:19 +0800

    feat: 项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: '龙趸之太平洋战争',
+  description: '龙趸（lóng dǔn），学名中巨石斑鱼，别名猪羔斑，为暖水性海洋底层鱼类，它呈长椭圆形，侧扁，口较大，鱼头部、体侧及各鳍均散布着很多青黑色斑点，为大型名贵食用鱼类',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+}
