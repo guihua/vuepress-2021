7be5ce812da723b85883c598abfbf885ff703555
龙趸 <guihuapeng@126.com>
2021-12-29 20:41:14 +0800

    chore: remove Element-plus

--- docs/.vuepress/clientAppEnhance.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client';
-
-// 引入 Element
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.use(ElementPlus);
-});
